6f4eb8ff9a237d4f2910529545e90c8ea5f0344d
Iain MacDonald <IJMacD@gmail.com>
2014-10-25 17:07:45 +0800

    Draw ports and snap with no rotation

--- js/project.js
+++ js/project.js
@@ -20,12 +20,58 @@ $(function() {
 				{
 					id: "zarya",
 					name: "Zarya",
-					image: "img/modules/ISS-Zarya.png"
+					image: "img/modules/ISS-Zarya.png",
+					ports: [
+						{
+							type: "CBM",
+							offsetX: -111,
+							offsetY: 0,
+							orientation: 3 * Math.PI * 0.5
+						},
+						{
+							type: "CBM",
+							offsetX: 106,
+							offsetY: 0,
+							orientation: 1 * Math.PI * 0.5
+						},
+						{
+							type: "CBM",
+							offsetX: 87,
+							offsetY: 20,
+							orientation: 2 * Math.PI * 0.5
+						}
+					]
 				},
 				{
 					id: "node1",
 					name: "Unity",
-					image: "img/modules/ISS-Unity.png"
+					image: "img/modules/ISS-Unity.png",
+					ports: [
+						{
+							type: "CBM",
+							offsetX: -49,
+							offsetY: 0,
+							orientation: 3 * Math.PI * 0.5
+						},
+						{
+							type: "CBM",
+							offsetX: 8,
+							offsetY: -38,
+							orientation: 0 * Math.PI * 0.5
+						},
+						{
+							type: "CBM",
+							offsetX: 47,
+							offsetY: 0,
+							orientation: 1 * Math.PI * 0.5
+						},
+						{
+							type: "CBM",
+							offsetX: 8,
+							offsetY: 38,
+							orientation: 2 * Math.PI * 0.5
+						}
+					]
 				},
 				{
 					id: "zvezda",
@@ -66,7 +112,9 @@ $(function() {
 
 
 			/* Parameters */
-			placingModule = false;
+			placingModule = false,
+
+			openPorts = [];
 
 	modules.forEach(function(item){
 		var li = $('<li>'+item.name+'</li>');
@@ -79,17 +127,18 @@ $(function() {
 				data = li.data("module");
 				x = e.pageX - canvas2.offset().left,
 				y = e.pageY - canvas2.offset().top;
-		placeModule(data.image, x, y);
+		placeModule(data, x, y);
 	});
 
-	function placeModule(img, x, y){
+	function placeModule(module, x, y){
 		placingModule = {x: x, y: y};
 
 		var imageObj = new Image();
 		imageObj.onload = function() {
 			requestAnimationFrame(drawImage)
 		};
-		imageObj.src = img;
+		imageObj.src = module.image;
+		module.imageObj = imageObj;
 
 		canvas2.on("mousemove", function(e){
 			placingModule.x = e.pageX - canvas2.offset().left;
@@ -97,10 +146,49 @@ $(function() {
 		});
 
 		canvas2.on("mousedown", function(e){
+			var targetPos = placingModule;
+
 			if(e.which == 1){
-				placingModule = null;
+
+
+				// find snap targets
+				if(module.ports){
+					module.ports.forEach(function(port){
+						openPorts.forEach(function(openPort){
+							var o = {
+								x: targetPos.x + port.offsetX,
+								y: targetPos.y + port.offsetY
+							}
+							if(dist(o, openPort) < 10){
+								targetPos.x = openPort.x - port.offsetX;
+								targetPos.y = openPort.y - port.offsetY;
+								return false;
+							}
+						});
+					});
+				}
+
+				// Commit module
+
+				drawModule(context, targetPos, module);
+
+				if(module.ports){
+					module.ports.forEach(function(port){
+						openPorts.push({
+							x: targetPos.x + port.offsetX,
+							y: targetPos.y + port.offsetY,
+							orientation: port.orientation
+						});
+					});
+				}
+
+				console.log(openPorts);
+
+				// Clean up
 				canvas2.off("mousemove mousedown contextmenu");
-				context.drawImage(canvas2[0], 0, 0);
+				canvas2.clearRect(0, 0, canvas2Width, canvas2Height);
+
+				placingModule = null;
 			}
 			else if(e.which == 3){
 				var r = placingModule.rotate || 0;
@@ -118,15 +206,49 @@ $(function() {
 		function drawImage(){
 			if(placingModule){
 				context2.clearRect(0, 0, canvas2Width, canvas2Height);
-				context2.save();
-				context2.translate(placingModule.x, placingModule.y)
-				if(placingModule.rotate){
-					context2.rotate(placingModule.rotate);
-				}
-				context2.drawImage(imageObj, -imageObj.width/2, -imageObj.height/2);
-				context2.restore();
+				drawModule(context2, placingModule, module);
 				requestAnimationFrame(drawImage);
 			}
 		}
+
+		function drawModule(ctx, pos, module){
+			ctx.save();
+			ctx.translate(pos.x, pos.y)
+			if(pos.rotate){
+				ctx.rotate(pos.rotate);
+			}
+			ctx.drawImage(module.imageObj, -module.imageObj.width/2, -module.imageObj.height/2);
+			drawPorts(ctx, module);
+			ctx.restore();
+		}
+
+		function drawPorts(ctx, module){
+			if(module.ports){
+				module.ports.forEach(function(port){
+					var CBM_WIDTH = 20;
+
+					var r = port.orientation,
+							demiPortWidth = CBM_WIDTH / 2,
+							sinr = Math.sin(r) * demiPortWidth,
+							cosr = Math.cos(r) * demiPortWidth,
+							x1 = port.offsetX - cosr,
+							y1 = port.offsetY - sinr,
+							x2 = port.offsetX + cosr,
+							y2 = port.offsetY + sinr;
+
+					ctx.lineWidth = 2;
+					ctx.strokeStyle = "#FF0000",
+
+					ctx.beginPath();
+					ctx.moveTo(x1, y1);
+					ctx.lineTo(x2, y2);
+					ctx.stroke();
+				});
+			}
+		}
+	}
+
+	function dist(a, b){
+		return Math.sqrt(Math.pow(a.x - b.x, 2) + Math.pow(a.y - b.y, 2));
 	}
 });
